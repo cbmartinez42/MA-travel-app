e079947f15503485c4493463b987f7fc210c81be
Mark Artim <mark.artim@gmail.com>
2021-06-27 16:12:03 -0500

    add palceholder pages and basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Pages/Home";
-import Admin from "./components/Pages/Admin"
+import About from "./Pages/About";
+import Admin from "./Pages/Admin";
+import Book from "./Pages/Book";
+import Browse from "./Pages/Browse";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Mystuff from "./Pages/Mystuff";
+import Operator from "./Pages/Operator";
+import Terms from "./Pages/Terms";
+import Tour from "./Pages/Tour";
+import Touradmin from "./Pages/Touradmin";
+
 // import { StoreProvider } from "./utils/GlobalState";  IF YOU NEED GLOBAL STUFF
 // import Nav from "./components/Nav";
 
 import './App.css';
-import Header from './components/Header'
+// import Header from './components/Header'
 
 function App() {
   return (
@@ -14,8 +24,17 @@ function App() {
         <div className="App">
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
+            <Route exact path="/about" component={About} />
             <Route exact path="/admin" component={Admin} />
+            <Route exact path="/book" component={Book} />
+            <Route exact path="/browse" component={Browse} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/mystuff" component={Mystuff} />
+            <Route exact path="/operator" component={Operator} />
+            <Route exact path="/terms" component={Terms} />
+            <Route exact path="/tour" component={Tour} />
+            <Route exact path="/touradmin" component={Touradmin} />
         </Switch>
         </div>
     </Router>
