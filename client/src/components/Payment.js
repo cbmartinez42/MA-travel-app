72b2964ebf1fa829a14b974454acb663207e7b9c
cbmartinez42 <cbmartinez42@gmail.com>
2021-07-10 20:22:46 -0500

    commenting out some unused dependancies

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import API from "../utils/API";
+// import Grid from "@material-ui/core/Grid";
+// import Button from "@material-ui/core/Button";
+// import API from "../utils/API";
 import ReactDOM from "react-dom"
 import paypal from 'paypal-checkout'
 
