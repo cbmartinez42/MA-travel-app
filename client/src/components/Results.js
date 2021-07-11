856760bb93c23583773f0b55556d1866e87aa350
cbmartinez42 <cbmartinez42@gmail.com>
2021-07-11 13:23:15 -0500

    adding new package, additional content and styling to tour details page

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 import API from '../utils/API'
 import {Box, Container} from '@material-ui/core/';
-import Button from "../components/Button"
+// import Button from "../components/Button"
 // import Tour from "../pages/Tour";
 import {Link} from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
@@ -23,32 +23,35 @@ const Results = ({searchData, setSearchData}) => {
 
     return (
         <>
-        <Container maxWidth="md">
-            {searchData.map(search => (
-                
-                <Box key={search._id} className="tour-abstract">
-                    <Box className="abstract-header">
-                        <h2>{search.tourName}</h2>
-                    </Box>
-                    <Grid container spacing={3}>
-                        <Grid item xs>
-                            <img alt="Tour" className="tour-thumbnail" src={search.image}></img>
-                        </Grid>
-                        <Grid item md>
-                            <p>Location: {search.tourLocation}</p>
-                            <p>Cost: ${search.cost}</p>
-                            <p>Operated by: {search.tourOperator}</p>
-                            <Link to={"/tour/" + search._id}>
-                                <Button 
-                                    onClick={() => renderDetail(search._id)}
-                                    text='Learn More!'
-                                />
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
-            )) }
-        </Container>
+            <Container maxWidth="md">
+                {searchData.map(search => (
+                   
+                    <Link className="abstract-link" to={"/tour/" + search._id}>
+                        <Box key={search._id} className="tour-abstract">
+                            <Box className="abstract-header">
+                                <h2>{search.tourName}</h2>
+                            </Box>
+                            <Grid container spacing={1}>
+                                <Grid item xs={12} md={4}>
+                                    <img alt="Tour" className="tour-thumbnail" src={search.image}></img>
+                                </Grid>
+                                <Grid item xs={12} md={8}>
+                                    <p>Location: {search.tourLocation}</p>
+                                    <p>Cost: ${search.cost}</p>
+                                    <p>Operated by: {search.tourOperator}</p>
+                                    
+                                        {/* <Button 
+                                            onClick={() => renderDetail(search._id)}
+                                            text='Learn More!'
+                                        /> */}
+                                    
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Link>
+             
+                ))}
+            </Container>
         </>
     )
 
