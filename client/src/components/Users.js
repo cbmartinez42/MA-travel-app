1397784123a30946e8d1195d39e9537f690dc57d
Mark Artim <mark.artim@gmail.com>
2021-07-13 17:42:46 -0500

    save user role part 1

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -5,9 +5,37 @@ import Button from "../components/Button"
 import {Link} from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+
+
+
 const UserData = ({searchUsers, setSearchUsers}) => {
 
     const [users, setUsers] = useState([])
+
+    const classes = useStyles();
+    const [role, setRole] = useState('');
+
+    const handleChange = (event) => {
+    setRole(event.target.value);
+    };
+
 
     useEffect(() => {
         API.browseAllUsers()
@@ -18,7 +46,7 @@ const UserData = ({searchUsers, setSearchUsers}) => {
         });
       }, [])
 
-    const renderDetail = (data) => {
+    const toggleRole = (data) => {
 
         console.log('data >> ', data)
     }
@@ -29,18 +57,24 @@ const UserData = ({searchUsers, setSearchUsers}) => {
             {users.map(user => (
                 
                 <Box key={user._id} className="tour-abstract">
-                    <Box className="abstract-header">
-                        <h2>{user.name.first} {user.name.last}</h2>
-                    </Box>
-                    <Grid container spacing={3}>
+                    {/* <Box>
+                        <p>{user.name.first} {user.name.last}</p>
+                    </Box> */}
+                    <Grid container spacing={1}>
                         <Grid item md>
-                            <p>Location: {user.address.city} {user.address.state}</p>
-                            <Link to={"/tour/" + user._id}>
-                                <Button 
-                                    onClick={() => renderDetail(user._id)}
-                                    text='Learn More!'
-                                />
-                            </Link>
+                            <p>{user.name.first} {user.name.last}    Location: {user.address.city} {user.address.state}    Role: {user.role}</p>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel id="user-role">Role</InputLabel>
+                                <Select
+                                labelId="user-role"
+                                className="user-role"
+                                value={user.role}
+                                onChange={handleChange}
+                                >
+                                <MenuItem value={10}>USER</MenuItem>
+                                <MenuItem value={20}>ADMIN</MenuItem>
+                                </Select>
+                            </FormControl>
                         </Grid>
                     </Grid>
                 </Box>
